f2cf86bd4dce87fc265612888f218f3aafbe3fbc
me7ell <964712654@qq.com>
2021-11-16 20:52:16 +0800

    Update indicators.py

--- indicators.py
+++ indicators.py
@@ -7,159 +7,163 @@ regex_indicators = '\\((.*?)(\\$_GET\\[.*?\\]|\\$_FILES\\[.*?\\]|\\$_POST\\[.*?\
 # Function_Name:String, Vulnerability_Name:String, Protection_Function:Array
 payloads = [
 
-    # Remote Command Execution
-    ["eval", "Remote Command Execution", ["escapeshellarg", "escapeshellcmd"]],
-    ["popen", "Remote Command Execution", ["escapeshellarg", "escapeshellcmd"]],
-    ["popen_ex", "Remote Command Execution", ["escapeshellarg", "escapeshellcmd"]],
-    ["system", "Remote Command Execution", ["escapeshellarg", "escapeshellcmd"]],
-    ["passthru", "Remote Command Execution", ["escapeshellarg", "escapeshellcmd"]],
-    ["exec", "Remote Command Execution", ["escapeshellarg", "escapeshellcmd"]],
-    ["shell_exec", "Remote Command Execution", ["escapeshellarg", "escapeshellcmd"]],
-    ["pcntl_exec", "Remote Command Execution", ["escapeshellarg", "escapeshellcmd"]],
-    ["assert", "Remote Command Execution", ["escapeshellarg", "escapeshellcmd"]],
-    ["proc_open", "Remote Command Execution", ["escapeshellarg", "escapeshellcmd"]],
-    ["expect_popen", "Remote Command Execution", ["escapeshellarg", "escapeshellcmd"]],
-    ["create_function", "Remote Command Execution", ["escapeshellarg", "escapeshellcmd"]],
-    ["call_user_func", "Remote Code Execution", []],
-    ["call_user_func_array", "Remote Code Execution", []],
-    ["preg_replace", "Remote Command Execution", ["preg_quote"]],
-    ["ereg_replace", "Remote Command Execution", ["preg_quote"]],
-    ["eregi_replace", "Remote Command Execution", ["preg_quote"]],
-    ["mb_ereg_replace", "Remote Command Execution", ["preg_quote"]],
-    ["mb_eregi_replace", "Remote Command Execution", ["preg_quote"]],
+    # RCE
+    ["eval", "RCE", ["escapeshellarg", "escapeshellcmd"]],
+    ["popen", "RCE", ["escapeshellarg", "escapeshellcmd"]],
+    ["popen_ex", "RCE", ["escapeshellarg", "escapeshellcmd"]],
+    ["system", "RCE", ["escapeshellarg", "escapeshellcmd"]],
+    ["passthru", "RCE", ["escapeshellarg", "escapeshellcmd"]],
+    ["exec", "RCE", ["escapeshellarg", "escapeshellcmd"]],
+    ["shell_exec", "RCE", ["escapeshellarg", "escapeshellcmd"]],
+    ["pcntl_exec", "RCE", ["escapeshellarg", "escapeshellcmd"]],
+    ["assert", "RCE", ["escapeshellarg", "escapeshellcmd"]],
+    ["proc_open", "RCE", ["escapeshellarg", "escapeshellcmd"]],
+    ["expect_popen", "RCE", ["escapeshellarg", "escapeshellcmd"]],
+    ["create_function", "RCE", ["escapeshellarg", "escapeshellcmd"]],
+    ["call_user_func", "RCE", []],
+    ["call_user_func_array", "RCE", []],
+    ["preg_replace", "RCE", ["preg_quote"]],
+    ["ereg_replace", "RCE", ["preg_quote"]],
+    ["eregi_replace", "RCE", ["preg_quote"]],
+    ["mb_ereg_replace", "RCE", ["preg_quote"]],
+    ["mb_eregi_replace", "RCE", ["preg_quote"]],
 
-    # File Inclusion / Path Traversal
-    ["virtual", "File Inclusion", []],
-    ["include", "File Inclusion", []],
-    ["require", "File Inclusion", []],
-    ["include_once", "File Inclusion", []],
-    ["require_once", "File Inclusion", []],
-
-    ["readfile", "File Inclusion / Path Traversal", []],
-    ["file_get_contents", "File Inclusion / Path Traversal", []],
-    ["file_put_contents", "File Inclusion / Path Traversal", []],
-    ["show_source", "File Inclusion / Path Traversal", []],
-    ["fopen", "File Inclusion / Path Traversal", []],
-    ["file", "File Inclusion / Path Traversal", []],
-    ["fpassthru", "File Inclusion / Path Traversal", []],
-    ["gzopen", "File Inclusion / Path Traversal", []],
-    ["gzfile", "File Inclusion / Path Traversal", []],
-    ["gzpassthru", "File Inclusion / Path Traversal", []],
-    ["readgzfile", "File Inclusion / Path Traversal", []],
+    # LFI
+    ["virtual", "LFI", []],
+    ["include", "LFI", []],
+    ["require", "LFI", []],
+    ["include_once", "LFI", []],
+    ["require_once", "LFI", []],
     
-    ["DirectoryIterator", "File Inclusion / Path Traversal", []],
-    ["stream_get_contents", "File Inclusion / Path Traversal", []],
-    ["copy", "File Inclusion / Path Traversal", []],
+    # PathTraversal
+    ["readfile", "PathTraversal", []],
+    ["file_get_contents", "PathTraversal", []],
+    ["file_put_contents", "PathTraversal", []],
+    ["show_source", "PathTraversal", []],
+    ["fopen", "PathTraversal", []],
+    ["file", "PathTraversal", []],
+    ["fpassthru", "PathTraversal", []],
+    ["gzopen", "PathTraversal", []],
+    ["gzfile", "PathTraversal", []],
+    ["gzpassthru", "PathTraversal", []],
+    ["readgzfile", "PathTraversal", []],
+    ["highlight_file", "PathTraversal", []],
+    ["unlink", "PathTraversal", []],
+    
+    ["DirectoryIterator", "PathTraversal", []],
+    ["stream_get_contents", "PathTraversal", []],
+    ["copy", "PathTraversal", []],
 
-    # MySQL(i) SQL Injection
-    ["mysql_query", "SQL Injection", ["mysql_real_escape_string"]],
-    ["mysqli_multi_query", "SQL Injection", ["mysql_real_escape_string"]],
-    ["mysqli_send_query", "SQL Injection", ["mysql_real_escape_string"]],
-    ["mysqli_master_query", "SQL Injection", ["mysql_real_escape_string"]],
-    ["mysqli_master_query", "SQL Injection", ["mysql_real_escape_string"]],
-    ["mysql_unbuffered_query", "SQL Injection", ["mysql_real_escape_string"]],
-    ["mysql_db_query", "SQL Injection", ["mysql_real_escape_string"]],
-    ["mysqli::real_query", "SQL Injection", ["mysql_real_escape_string"]],
-    ["mysqli_real_query", "SQL Injection", ["mysql_real_escape_string"]],
-    ["mysqli::query", "SQL Injection", ["mysql_real_escape_string"]],
-    ["mysqli_query", "SQL Injection", ["mysql_real_escape_string"]],
+    # MySQL(i) SQLI
+    ["mysql_query", "SQLI", ["mysql_real_escape_string"]],
+    ["mysqli_multi_query", "SQLI", ["mysql_real_escape_string"]],
+    ["mysqli_send_query", "SQLI", ["mysql_real_escape_string"]],
+    ["mysqli_master_query", "SQLI", ["mysql_real_escape_string"]],
+    ["mysqli_master_query", "SQLI", ["mysql_real_escape_string"]],
+    ["mysql_unbuffered_query", "SQLI", ["mysql_real_escape_string"]],
+    ["mysql_db_query", "SQLI", ["mysql_real_escape_string"]],
+    ["mysqli::real_query", "SQLI", ["mysql_real_escape_string"]],
+    ["mysqli_real_query", "SQLI", ["mysql_real_escape_string"]],
+    ["mysqli::query", "SQLI", ["mysql_real_escape_string"]],
+    ["mysqli_query", "SQLI", ["mysql_real_escape_string"]],
 
-    # PostgreSQL Injection
-    ["pg_query", "SQL Injection", ["pg_escape_string", "pg_pconnect", "pg_connect"]],
-    ["pg_send_query", "SQL Injection", ["pg_escape_string", "pg_pconnect", "pg_connect"]],
+    # Postgre SQLI
+    ["pg_query", "SQLI", ["pg_escape_string", "pg_pconnect", "pg_connect"]],
+    ["pg_send_query", "SQLI", ["pg_escape_string", "pg_pconnect", "pg_connect"]],
 
-    # SQLite SQL Injection
-    ["sqlite_array_query", "SQL Injection", ["sqlite_escape_string"]],
-    ["sqlite_exec", "SQL Injection", ["sqlite_escape_string"]],
-    ["sqlite_query", "SQL Injection", ["sqlite_escape_string"]],
-    ["sqlite_single_query", "SQL Injection", ["sqlite_escape_string"]],
-    ["sqlite_unbuffered_query", "SQL Injection", ["sqlite_escape_string"]],
+    # SQLite SQLI
+    ["sqlite_array_query", "SQLI", ["sqlite_escape_string"]],
+    ["sqlite_exec", "SQLI", ["sqlite_escape_string"]],
+    ["sqlite_query", "SQLI", ["sqlite_escape_string"]],
+    ["sqlite_single_query", "SQLI", ["sqlite_escape_string"]],
+    ["sqlite_unbuffered_query", "SQLI", ["sqlite_escape_string"]],
 
-    # PDO SQL Injection
-    ["->arrayQuery", "SQL Injection", ["->prepare"]],
-    ["->query", "SQL Injection", ["->prepare"]],
-    ["->queryExec", "SQL Injection", ["->prepare"]],
-    ["->singleQuery", "SQL Injection", ["->prepare"]],
-    ["->querySingle", "SQL Injection", ["->prepare"]],
-    ["->exec", "SQL Injection", ["->prepare"]],
-    ["->execute", "SQL Injection", ["->prepare"]],
-    ["->unbufferedQuery", "SQL Injection", ["->prepare"]],
-    ["->real_query", "SQL Injection", ["->prepare"]],
-    ["->multi_query", "SQL Injection", ["->prepare"]],
-    ["->send_query", "SQL Injection", ["->prepare"]],
+    # PDO SQLI
+    ["->arrayQuery", "SQLI", ["->prepare"]],
+    ["->query", "SQLI", ["->prepare"]],
+    ["->queryExec", "SQLI", ["->prepare"]],
+    ["->singleQuery", "SQLI", ["->prepare"]],
+    ["->querySingle", "SQLI", ["->prepare"]],
+    ["->exec", "SQLI", ["->prepare"]],
+    ["->execute", "SQLI", ["->prepare"]],
+    ["->unbufferedQuery", "SQLI", ["->prepare"]],
+    ["->real_query", "SQLI", ["->prepare"]],
+    ["->multi_query", "SQLI", ["->prepare"]],
+    ["->send_query", "SQLI", ["->prepare"]],
 
-    # Cubrid SQL Injection
-    ["cubrid_unbuffered_query", "SQL Injection", ["cubrid_real_escape_string"]],
-    ["cubrid_query", "SQL Injection", ["cubrid_real_escape_string"]],
+    # Cubrid SQLI
+    ["cubrid_unbuffered_query", "SQLI", ["cubrid_real_escape_string"]],
+    ["cubrid_query", "SQLI", ["cubrid_real_escape_string"]],
 
-    # MSSQL SQL Injection : Warning there is not any real_escape_string
-    ["mssql_query", "SQL Injection", ["mssql_escape"]],
+    # MSSQL SQLI : Warning there is not any real_escape_string
+    ["mssql_query", "SQLI", ["mssql_escape"]],
 
-    # File Upload
-    ["move_uploaded_file", "File Upload", []],
+    # FileUpload
+    ["move_uploaded_file", "FileUpload", []],
 
     # Cross Site Scripting
-    ["echo", "Cross Site Scripting", ["htmlentities", "htmlspecialchars"]],
-    ["print", "Cross Site Scripting", ["htmlentities", "htmlspecialchars"]],
-    ["printf", "Cross Site Scripting", ["htmlentities", "htmlspecialchars"]],
-    ["vprintf", "Cross Site Scripting", ["htmlentities", "htmlspecialchars"]],
-    ["trigger_error", "Cross Site Scripting", ["htmlentities", "htmlspecialchars"]],
-    ["user_error", "Cross Site Scripting", ["htmlentities", "htmlspecialchars"]],
-    ["odbc_result_all", "Cross Site Scripting", ["htmlentities", "htmlspecialchars"]],
-    ["ifx_htmltbl_result", "Cross Site Scripting", ["htmlentities", "htmlspecialchars"]],
-    ["die", "Cross Site Scripting", ["htmlentities", "htmlspecialchars"]],
-    ["exit", "Cross Site Scripting", ["htmlentities", "htmlspecialchars"]],
-    ["var_dump", "Cross Site Scripting", ["htmlentities", "htmlspecialchars"]],
+    # ["echo", "Cross Site Scripting", ["htmlentities", "htmlspecialchars"]],
+    # ["print", "Cross Site Scripting", ["htmlentities", "htmlspecialchars"]],
+    # ["printf", "Cross Site Scripting", ["htmlentities", "htmlspecialchars"]],
+    # ["vprintf", "Cross Site Scripting", ["htmlentities", "htmlspecialchars"]],
+    # ["trigger_error", "Cross Site Scripting", ["htmlentities", "htmlspecialchars"]],
+    # ["user_error", "Cross Site Scripting", ["htmlentities", "htmlspecialchars"]],
+    # ["odbc_result_all", "Cross Site Scripting", ["htmlentities", "htmlspecialchars"]],
+    # ["ifx_htmltbl_result", "Cross Site Scripting", ["htmlentities", "htmlspecialchars"]],
+    # ["die", "Cross Site Scripting", ["htmlentities", "htmlspecialchars"]],
+    # ["exit", "Cross Site Scripting", ["htmlentities", "htmlspecialchars"]],
+    # ["var_dump", "Cross Site Scripting", ["htmlentities", "htmlspecialchars"]],
 
     # XPATH and LDAP
-    ["xpath", "XPATH Injection", []],
-    ["ldap_search", "LDAP Injection", ["Zend_Ldap", "ldap_escape"]],
+    # ["xpath", "XPATH Injection", []],
+    # ["ldap_search", "LDAP Injection", ["Zend_Ldap", "ldap_escape"]],
 
     # Insecure E-Mail
-    ["mail", "Insecure E-mail", []],
+    # ["mail", "Insecure E-mail", []],
 
-    # PHP Objet Injection
-    ["unserialize", "PHP Object Injection", []],
+    # PHP unserialize
+    ["unserialize", "unserialize", []],
 
     # Header Injection
-    ["header", "Header Injection", []],
-    ["HttpMessage::setHeaders", "Header Injection", []],
-    ["HttpRequest::setHeaders", "Header Injection", []],
+    # ["header", "Header Injection", []],
+    # ["HttpMessage::setHeaders", "Header Injection", []],
+    # ["HttpRequest::setHeaders", "Header Injection", []],
 
     # URL Redirection
-    ["http_redirect", "URL Redirection", []],
-    ["HttpMessage::setResponseCode", "URL Redirection", []],
+    # ["http_redirect", "URL Redirection", []],
+    # ["HttpMessage::setResponseCode", "URL Redirection", []],
 
-    # Server Side Template Injection
-    ["->render", "Server Side Template Injection", []],
-    ["->assign", "Server Side Template Injection", []],
-
+    # SSTI
+    ["->render", "SSTI", []],
+    ["->assign", "SSTI", []],
+    ["->fetch", "SSTI", []], #文件包含
+    
     # Weak Cryptographic Hash
-    ["md5", "Weak Cryptographic Hash", []],
-    ["sha1", "Weak Cryptographic Hash", []],
+    # ["md5", "Weak Cryptographic Hash", []],
+    # ["sha1", "Weak Cryptographic Hash", []],
 
-    # Insecure Weak Random
-    ["mt_rand", "Insecure Weak Random", []],
-    ["srand", "Insecure Weak Random", []],
-    ["uniqid", "Insecure Weak Random", []],
+    # # Insecure Weak Random
+    # ["mt_rand", "Insecure Weak Random", []],
+    # ["srand", "Insecure Weak Random", []],
+    # ["uniqid", "Insecure Weak Random", []],
 
     # Information Leak
-    ["phpinfo", "Information Leak", []],
-    ["debug_print_backtrace", "Information Leak", []],
-    ["show_source", "Information Leak", []],
-    ["highlight_file", "Information Leak", []],
+    # ["phpinfo", "Information Leak", []],
+    # ["debug_print_backtrace", "Information Leak", []],
+    # ["show_source", "Information Leak", []],
+    # ["highlight_file", "Information Leak", []],
 
-    # Server Side Request Forgery
-    ["curl_setopt", "Server Side Request Forgery", []],
-    ["curl_exec", "Server Side Request Forgery", []],
-    ["fsockopen", "Server Side Request Forgery", []],
+    # SSRF
+    ["curl_setopt", "SSRF", []],
+    ["curl_exec", "SSRF", []],
+    ["fsockopen", "SSRF", []],
 
 
-    # XML External Entity
-    ["SimpleXMLElement", "XML External Entity", []],
-    ["xmlparse", "XML External Entity", []],
-    ["loadXML", "XML External Entity", []],
-    ["simplexml_load_string", "XML External Entity", []],
+    # XXE
+    ["SimpleXMLElement", "XXE", []],
+    ["xmlparse", "XXE", []],
+    ["loadXML", "XXE", []],
+    ["simplexml_load_string", "XXE", []],
 
     # Others
     ["unlink", "Arbitrary File Deletion", []],
